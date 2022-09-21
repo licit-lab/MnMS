f1023a5d3061dfa7c360a5861540c5ff24151176
Mélanie Cortina <melanie.cortina.dev@gmail.com>
2022-09-21 16:48:20 +0200

    Add draw_line (for public transport lines) and draw_odlayer rendering tools.

--- src/mnms/tools/render.py
+++ src/mnms/tools/render.py
@@ -43,3 +43,38 @@ def draw_path(ax, mlgraph, path, color='red', linkwidth=2, alpha=1):
 
         line_segment = LineCollection(lines, linestyles='solid', colors=color, linewidths=linkwidth, alpha=alpha)
         ax.add_collection(line_segment)
+
+def draw_line(ax, mlgraph, line, color='green', linkwidth=6, alpha=0.6, draw_stops=True,
+    nodesize=6, line_label='', label_size=5):
+    lines = list()
+    starting_stop = mlgraph.roads.stops[line['stops'][0]]
+    ending_stop = mlgraph.roads.stops[line['stops'][-1]]
+    for i,sections in enumerate(line['sections']):
+        for j,section in enumerate(sections):
+            section_data = mlgraph.roads.sections[section]
+            unode_pos = mlgraph.roads.nodes[section_data.upstream].position
+            dnode_pos = mlgraph.roads.nodes[section_data.downstream].position
+            if i == 0 and j == 0:
+                # Use strating stop position as unode
+                unode_pos = starting_stop.absolute_position
+            if i == len(line['sections'])-1 and j == len(sections)-1:
+                # Use ending stop position as dnode
+                dnode_pos = ending_stop.absolute_position
+            lines.append([unode_pos, dnode_pos])
+    line_segment = LineCollection(lines, linestyles='solid', colors=color,
+        linewidths=linkwidth, alpha=alpha)
+    ax.add_collection(line_segment)
+
+    if line_label != '':
+        ax.annotate(line_label, starting_stop.absolute_position, size=label_size)
+
+    if draw_stops:
+        x, y = zip(*[mlgraph.roads.stops[stop].absolute_position.tolist() for stop in line['stops']])
+        ax.plot(x, y, 'o', markerfacecolor=color, markeredgecolor='black',
+            fillstyle='full', markersize=nodesize)
+
+def draw_odlayer(ax, mlgraph, color='blue', nodesize=2):
+    ods = list(mlgraph.odlayer.origins.keys()) + list(mlgraph.odlayer.destinations.keys())
+    x, y = zip(*[mlgraph.graph.nodes[od].position for od in ods])
+    ax.plot(x, y, 'o', markerfacecolor=color, markeredgecolor='black',
+        fillstyle='full', markersize=nodesize)
