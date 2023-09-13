405cb4168ce1515cf060131880471a99b4d3a5ee
Christophe Tettarassar <tetta.chris@gmail.com>
2023-09-13 11:27:11 +0200

    Improve demand validation
    Improve standalone demand validation script:
    - file path as argument
    - percentage validation info

--- script/validation/validate_demand.py
+++ script/validation/validate_demand.py
@@ -1,7 +1,34 @@
+import os
+import argparse
 import pandas as pd
 
 from datetime import datetime
 
+
+def validate_demand(file):
+    df_users = pd.read_csv(file, sep=';')
+
+    invalid_users_count = 0
+
+    for index, user in df_users.iterrows():
+        user_valid = True
+
+        user_valid = validate_user_id(user)
+        user_valid = validate_user_departure_time(user)
+        user_valid = validate_user_origin(user)
+        user_valid = validate_user_destination(user)
+        user_valid = validate_user_journey(user)
+
+        if not user_valid:
+            print(f"User: {user} invalid")
+            invalid_users_count = invalid_users_count + 1
+
+    total_users = len(df_users)
+    validation = 100 - invalid_users_count * 100 / total_users
+
+    print(f"Total number of users: {len(df_users)}")
+    print(f"Number of invalid users: {invalid_users_count}")
+    print(f"Validation : {validation}%")
 
 
 def validate_demand_columns(df_users):
@@ -59,11 +86,11 @@ def validate_user_id(user):
     if user[0]:
         user_id = user[0]
         if not isinstance(user_id, int):
-            print(f"Invalid user id for user: {user}")
+            print(f"Invalid user id for user: {user[0]}")
             valid = False
 
         if not user_id > 0:
-            print(f"Invalid user id for user: {user}")
+            print(f"Invalid user id for user: {user[0]}")
             valid = False
     else:
         print(f"No id found for user: {user}")
@@ -93,10 +120,10 @@ def validate_user_departure_time(user):
         try:
             datetime.strptime(departure_time, time_format)
         except ValueError:
-            print(f"Invalid departure time for user: {user}")
+            print(f"Invalid departure time for user: {user[0]}")
             valid = False
     else:
-        print(f"No departure time found for user: {user}")
+        print(f"No departure time found for user: {user[0]}")
         valid = False
 
     return valid
@@ -120,7 +147,7 @@ def validate_user_origin(user):
     if user[2]:
         user_origin = user[2]
     else:
-        print(f"No origin found for user: {user}")
+        print(f"No origin found for user: {user[0]}")
         valid = False
 
     return valid
@@ -144,7 +171,7 @@ def validate_user_destination(user):
     if user[3]:
         user_destination = user[3]
     else:
-        print(f"No destination found for user: {user}")
+        print(f"No destination found for user: {user[0]}")
         valid = False
 
     return valid
@@ -166,7 +193,7 @@ def validate_user_journey(user):
     valid = True
 
     if user[2] == user[3]:
-        print(f"Warning: origin equals destination for user {user}")
+        print(f"Warning: origin equals destination for user {user[0]}, origin = destination = {user[2]}")
 
     return valid
 
@@ -188,24 +215,16 @@ def check_user_id_duplicates(users):
 
     return valid
 
+def _path_file_type(path):
+    if os.path.isfile(path):
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"{path} is not a valid path")
 
-demand_csv_filepath = "test.csv"
-df_users = pd.read_csv(demand_csv_filepath, sep=';')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Validate a CSV demand file for MnMS")
+    parser.add_argument('demand_file', type=_path_file_type, help='Path to the demand csv file')
 
-invalid_users_count = 0
+    args = parser.parse_args()
 
-for index, user in df_users.iterrows():
-    user_valid = True
-
-    user_valid = validate_user_id(user)
-    user_valid = validate_user_departure_time(user)
-    user_valid = validate_user_origin(user)
-    user_valid = validate_user_destination(user)
-    user_valid = validate_user_journey(user)
-
-    if not user_valid:
-        print(f"User: {user} invalid")
-        invalid_users_count = invalid_users_count + 1
-
-print(f"Total number of users: {len(df_users)}")
-print(f"Number of invalid users: {invalid_users_count}")
+    validate_demand(args.demand_file)
