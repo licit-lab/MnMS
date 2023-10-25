ef82588ad97929c906e179e5627f15eb005785f1
Cécile BECARIE <cecile.becarie@ifsttar.fr>
2023-10-25 10:17:04 +0200

    Update public transport example

--- examples/public_transport/run.py
+++ examples/public_transport/run.py
@@ -8,7 +8,7 @@ from mnms.generation.layers import generate_matching_origin_destination_layer, g
 from mnms.generation.roads import generate_line_road, generate_manhattan_road
 from mnms.graph.layers import PublicTransportLayer, MultiLayerGraph
 from mnms.io.graph import save_graph, load_graph
-from mnms.log import set_mnms_logger_level
+from mnms.log import set_mnms_logger_level, attach_log_file
 from mnms.mobility_service.public_transport import PublicTransportMobilityService
 from mnms.simulation import Supervisor
 from mnms.time import TimeTable, Time, Dt
@@ -25,6 +25,7 @@ set_mnms_logger_level(LOGLEVEL.INFO, ['mnms.simulation',
                                       # 'mnms.travel_decision.dummy',
                                       'mnms.tools.observer'
                                       ])
+attach_log_file('simulation.log')
 
 cwd = pathlib.Path(__file__).parent.joinpath('demand.csv').resolve()
 
@@ -39,12 +40,12 @@ roads.register_stop('SD', '0_1', 0.9)
 bus_service = PublicTransportMobilityService('B0')
 
 veh = pathlib.Path(__file__).parent.joinpath('veh.csv').resolve()
-pblayer = PublicTransportLayer(roads, 'BUS', Bus, 13, services=[bus_service], observer=CSVVehicleObserver(veh))
+pblayer = PublicTransportLayer(roads, 'BUS', Bus, 10, services=[bus_service], observer=CSVVehicleObserver(veh))
 
 pblayer.create_line('L0',
                     ['SO', 'S1', 'S2','SD'],
                     [['0_1'], ['0_1'],['0_1']],
-                    TimeTable.create_table_freq('07:00:00', '08:00:00', Dt(minutes=10)))
+                    TimeTable.create_table_freq('07:01:00', '08:01:00', Dt(minutes=5)))
 
 odlayer = generate_matching_origin_destination_layer(roads)
 
@@ -55,9 +56,8 @@ mlgraph = MultiLayerGraph([pblayer],
                           200)
 
 # Demand
-
-#demand = CSVDemandManager(cwd)
-#demand.add_user_observer(CSVUserObserver('user.csv'))
+demand = CSVDemandManager(cwd)
+demand.add_user_observer(CSVUserObserver('user.csv'))
 
 # Decison Model
 
@@ -73,11 +73,11 @@ flow_motor = MFDFlowMotor()
 flow_motor.add_reservoir(Reservoir(roads.zones['RES'], ['BUS'], mfdspeed))
 
 supervisor = Supervisor(mlgraph,
-                        None,
+                        demand,
                         flow_motor,
                         decision_model)
 
 supervisor.run(Time("07:00:00"),
-               Time("08:00:10"),
+               Time("08:30:00"),
                Dt(minutes=1),
-               1)
+               10)
