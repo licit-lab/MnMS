bb0fee5a85c3377c22a6142c339884d0677b8af5
Mélanie Cortina <melanie.cortina.dev@gmail.com>
2023-11-23 14:31:16 +0100

    Add optional arg polygon to create a grid od layer in a bindbox-polygon intersection.

--- src/mnms/generation/layers.py
+++ src/mnms/generation/layers.py
@@ -1,4 +1,4 @@
-from typing import Optional, Type, List
+from typing import Optional, Type, List, Annotated
 
 import numpy as np
 
@@ -6,8 +6,11 @@ from mnms.graph.abstract import AbstractLayer
 from mnms.graph.layers import OriginDestinationLayer, SimpleLayer
 from mnms.graph.road import RoadDescriptor
 from mnms.mobility_service.abstract import AbstractMobilityService
-from mnms.tools.geometry import get_bounding_box
+from mnms.tools.geometry import get_bounding_box, points_in_polygon
 from mnms.vehicles.veh_type import Vehicle, Car
+
+Point = Annotated[List[float], 2]
+PointList = List[Point]
 
 
 def generate_layer_from_roads(roads: RoadDescriptor,
@@ -80,7 +83,8 @@ def generate_grid_origin_destination_layer(xmin: float,
                                            xmax: float,
                                            ymax: float,
                                            nx: int,
-                                           ny: Optional[int] = None):
+                                           ny: Optional[int] = None,
+                                           polygon: Optional[PointList] = None):
     """
     Generate a rectangular structured grid for the OriginDestinationLayer
 
@@ -91,6 +95,8 @@ def generate_grid_origin_destination_layer(xmin: float,
         ymax: the max y of the grid
         nx: the number of point in the x direction
         ny: the number of point in the y direction
+        polygon: optional arg that specifies a polygon outside of which the
+                 origin and destination should not be created
 
     Returns:
         The OriginDestinationLayer
@@ -110,13 +116,16 @@ def generate_grid_origin_destination_layer(xmin: float,
     for j in range(ny):
         for i in range(nx):
             pos = np.array([xmin + i * dx, ymin + j * dy])
+            if polygon is not None:
+                if not points_in_polygon(np.array(polygon), [pos])[0]:
+                    continue
             odlayer.create_destination_node(f"DESTINATION_{str(i + j * nx)}", pos)
             odlayer.create_origin_node(f"ORIGIN_{str(i + j * nx)}", pos)
 
     return odlayer
 
 
-def generate_bbox_origin_destination_layer(roads: RoadDescriptor, nx: int, ny: Optional[int] = None) -> OriginDestinationLayer:
+def generate_bbox_origin_destination_layer(roads: RoadDescriptor, nx: int, ny: Optional[int] = None, polygon: Optional[PointList] = None) -> OriginDestinationLayer:
     """
     Generate a grid OriginDestinationLayer based on the bounding box of the roads nodes
 
@@ -124,10 +133,11 @@ def generate_bbox_origin_destination_layer(roads: RoadDescriptor, nx: int, ny: O
         roads: The OriginDestinationLayer from which the odlayer is created
         nx: The number of point in the x direction
         ny: The number of point in the y direction
+        polygon: Optional arg that specifies a polygon outside of which the origins and destinations are not created
 
     Returns:
         The generated layer
 
     """
     bbox = get_bounding_box(roads)
-    return generate_grid_origin_destination_layer(bbox.xmin - 1, bbox.ymin - 1, bbox.xmax + 1, bbox.ymax + 1, nx, ny)
+    return generate_grid_origin_destination_layer(bbox.xmin - 1, bbox.ymin - 1, bbox.xmax + 1, bbox.ymax + 1, nx, ny, polygon=polygon)
