71530003b0d707d449826adbdaf6ccebe52b226d
Mélanie Cortina <melanie.cortina.dev@gmail.com>
2024-02-19 17:08:52 +0100

    Clean and fix the simple examples.

--- examples/manhattan/run.py
+++ examples/manhattan/run.py
@@ -9,24 +9,22 @@ from mnms.generation.roads import generate_manhattan_road
 from mnms.generation.layers import generate_layer_from_roads, generate_grid_origin_destination_layer
 from mnms.graph.layers import MultiLayerGraph
 from mnms.demand.manager import CSVDemandManager
-from mnms.io.graph import save_graph
 from mnms.log import set_all_mnms_logger_level, LOGLEVEL
 from mnms.travel_decision.dummy import DummyDecisionModel
-from mnms.mobility_service.on_demand import OnDemandDepotMobilityService
+from mnms.mobility_service.personal_vehicle import PersonalMobilityService
 from mnms.flow.MFD import MFDFlowMotor, Reservoir
 from mnms.simulation import Supervisor
 from mnms.time import Time, Dt
 from mnms.tools.observer import CSVUserObserver, CSVVehicleObserver
-from mnms.mobility_service.personal_vehicle import PersonalMobilityService
 
 ##################
 ### Parameters ###
 ##################
+
 demand_file = pathlib.Path(__file__).parent.joinpath('demand.csv').resolve()
 log_file = pathlib.Path(__file__).parent.joinpath('sim.log').resolve()
 n_nodes_per_dir = 3
 mesh_size = 100 # m
-uber_dt_matching = 0
 n_odnodes_x = 3
 n_odnodes_y = 3
 odlayer_xmin = 0
@@ -34,24 +32,13 @@ odlayer_ymin = 0
 odlayer_xmax = 300 # m
 odlayer_ymax = 300 # m
 odlayer_connection_dist = 1e-3 # m
-def gc_pv(mlgraph, link, costs, car_kmcost=0.0005, vot=0.003):
-    gc = link.length * car_kmcost + vot * link.length / costs["PV"]['speed']
-    return gc
-def gc_uber(mlgraph, link, costs, uber_kmcost=0.0001, vot=0.003):
-    gc = link.length * uber_kmcost + vot * link.length / costs["UBER"]['speed']
-    return gc
-def gc_transit(mlgraph, link, costs, vot=0.003):
-    gc = vot * link.length / costs["WALK"]['speed']
-    return gc
-def gc_waiting(wt, vot=0.003):
-    return vot * wt
 def mfdspeed(dacc):
-    dspeed = {'CAR': 3}
+    dspeed = {'CAR': 3} # m/s
     return dspeed
-tstart = Time("06:59:00")
-tend = Time("08:00:00")
-dt_flow = Dt(minutes=1)
-affectation_factor = 1
+tstart = Time("07:00:00")
+tend = Time("07:03:03")
+dt_flow = Dt(seconds=10)
+affectation_factor = 10
 
 #########################
 ### Scenario creation ###
@@ -61,15 +48,11 @@ affectation_factor = 1
 road_db = generate_manhattan_road(n_nodes_per_dir, mesh_size) # one zone automatically generated
 
 #### MlGraph ####
-uber = OnDemandDepotMobilityService("UBER", uber_dt_matching)
-uber.attach_vehicle_observer(CSVVehicleObserver("uber_vehs.csv"))
-
-pv = PersonalMobilityService('PV')
-pv.attach_vehicle_observer(CSVVehicleObserver("pv_vehs.csv"))
-
+personal_car = PersonalMobilityService()
+personal_car.attach_vehicle_observer(CSVVehicleObserver("veh.csv"))
 car_layer = generate_layer_from_roads(road_db,
                                       'CAR',
-                                      mobility_services=[pv,uber])
+                                      mobility_services=[personal_car])
 
 odlayer = generate_grid_origin_destination_layer(odlayer_xmin, odlayer_ymin, odlayer_xmax,
     odlayer_ymax, n_odnodes_x, n_odnodes_y)
@@ -78,29 +61,21 @@ mlgraph = MultiLayerGraph([car_layer],
                           odlayer,
                           odlayer_connection_dist)
 
-mlgraph.add_cost_function('CAR', 'generalized_cost', gc_pv, mobility_service='PV')
-mlgraph.add_cost_function('CAR', 'generalized_cost', gc_uber, mobility_service='UBER')
-mlgraph.add_cost_function('TRANSIT', 'generalized_cost', gc_transit)
-
-#save_graph(mlgraph, demand_file.parent.joinpath('graph.json'))
+# save_graph(mlgraph, demand_file.parent.joinpath('graph.json'))
 
 # load_graph(demand_file.parent.joinpath('graph.json'))
 
-# uber.create_waiting_vehicle("CAR_1") # creation of 1 vehicle outside of any depot at CAR_1 node
-uber.add_depot("CAR_1", 1) # creation of 1 depot at CAR_1 node with 1 vehicle inside
 
 #### Demand ####
 demand = CSVDemandManager(demand_file)
 demand.add_user_observer(CSVUserObserver('user.csv'))
 
 #### Decision model ####
-decision_model = DummyDecisionModel(mlgraph, cost='generalized_cost',
-    outfile="path.csv", verbose_file=True)
-decision_model.add_waiting_cost_function('generalized_cost', gc_waiting)
+decision_model = DummyDecisionModel(mlgraph, outfile="path.csv")
 
 #### Flow motor ####
 flow_motor = MFDFlowMotor()
-flow_motor.add_reservoir(Reservoir(road_db.zones["RES"], ['CAR'], mfdspeed))
+flow_motor.add_reservoir(Reservoir(road_db.zones["RES"], 'CAR', mfdspeed))
 
 #### Supervisor ####
 supervisor = Supervisor(mlgraph,
--- examples/vehicle_sharing/run_intermodality.py
+++ examples/vehicle_sharing/run_intermodality.py
@@ -6,9 +6,10 @@ import pathlib
 
 # MnMS
 from mnms.generation.roads import generate_manhattan_road
-from mnms.mobility_service.vehicle_sharing import VehicleSharingMobilityService
-from mnms.tools.observer import CSVVehicleObserver, CSVUserObserver
 from mnms.generation.layers import generate_layer_from_roads, generate_grid_origin_destination_layer
+from mnms.mobility_service.vehicle_sharing import VehicleSharingMobilityService
+from mnms.mobility_service.personal_vehicle import PersonalMobilityService
+from mnms.tools.observer import CSVVehicleObserver, CSVUserObserver
 from mnms.graph.layers import SharedVehicleLayer, MultiLayerGraph
 from mnms.vehicles.veh_type import Bike
 from mnms.travel_decision.dummy import DummyDecisionModel
@@ -19,14 +20,13 @@ from mnms.time import Time, Dt
 from mnms.log import set_mnms_logger_level, LOGLEVEL, attach_log_file
 from mnms.io.graph import save_graph, load_graph
 
-
 ##################
 ### Parameters ###
 ##################
-b_from_json=False
-mlgraph_file = 'velov_example.json'
 demand_file = pathlib.Path(__file__).parent.joinpath('demand.csv').resolve()
+mlgraph_file = 'intermodality_example.json'
 log_file = 'sim.log'
+b_from_json=False
 n_nodes_per_dir = 3
 mesh_size = 1000 # m
 n_odnodes_x = 5
@@ -34,13 +34,14 @@ n_odnodes_y = 5
 odlayer_xmin = -1000
 odlayer_ymin = -1000
 odlayer_xmax = 3000 # m
-odlayer_ymax = 3000 # m
-odlayer_connection_dist = 500 # m
-velov_default_speed = 3 # m/s
+odlayer_ymax = 3000 # m$
 b_freefloating = 0
 velov_dt_matching = 1
+velov_default_speed = 3 # m/s
+odlayer_connection_dist = 500 # m
+car_velov_connection_dist = 300 # m
 def mfdspeed(dacc):
-    dspeed = {'BIKE': 3}
+    dspeed = {'CAR':5.55, 'BIKE': 6.66}
     return dspeed
 tstart = Time("06:50:00")
 tend = Time("09:00:00")
@@ -67,31 +68,40 @@ else:
     odlayer = generate_grid_origin_destination_layer(odlayer_xmin, odlayer_ymin,
         odlayer_xmax, odlayer_ymax, n_odnodes_x, n_odnodes_y)
 
+    # Personnal car mobility service
+    personal_car = PersonalMobilityService()
+    personal_car.attach_vehicle_observer(CSVVehicleObserver("pv_vehs.csv"))
+    car_layer = generate_layer_from_roads(road_db,
+                                          'car_layer',
+                                          mobility_services=[personal_car])
+
     # Vehicle sharing mobility service
     velov = VehicleSharingMobilityService("velov", b_freefloating, velov_dt_matching)
     velov.attach_vehicle_observer(CSVVehicleObserver("velov_vehs.csv"))
-    velov_layer = generate_layer_from_roads(road_db, 'velov_layer', SharedVehicleLayer,
-        Bike, velov_default_speed, [velov])
+    velov_layer = generate_layer_from_roads(road_db, 'velov_layer', SharedVehicleLayer, Bike, velov_default_speed, [velov])
 
     # Multilayer graph
-    mlgraph = MultiLayerGraph([velov_layer], odlayer)
+    mlgraph = MultiLayerGraph([car_layer,velov_layer], odlayer)
 
     if not b_from_json:
         save_graph(mlgraph, mlgraph_file)
 
 # Add stations
-mlgraph.layers['velov_layer'].mobility_services['velov'].create_station('S1', '2', capacity=20, nb_initial_veh=5)
+mlgraph.layers['velov_layer'].mobility_services['velov'].create_station('S1', '1', capacity=20, nb_initial_veh=5)
 mlgraph.layers['velov_layer'].mobility_services['velov'].create_station('S2', 'SOUTH_2', capacity=20, nb_initial_veh=0)
 
 # Connect od layer and velov layer
 mlgraph.connect_origindestination_layers(odlayer_connection_dist)
 
+# Connect personnal car layer and velov layer
+mlgraph.connect_inter_layers(['car_layer','velov_layer'], car_velov_connection_dist)
+
 #### Decision model ####
-decision_model = DummyDecisionModel(mlgraph, outfile="paths.csv")
+decision_model = DummyDecisionModel(mlgraph, outfile="paths.csv", verbose_file=True)
 
 #### Flow motor ####
 flow_motor = MFDFlowMotor(outfile="flow.csv")
-flow_motor.add_reservoir(Reservoir(mlgraph.roads.zones['RES'], ['BIKE'], mfdspeed))
+flow_motor.add_reservoir(Reservoir(mlgraph.roads.zones['RES'], ['BIKE', 'CAR'], mfdspeed))
 
 #### Demand ####
 demand = CSVDemandManager(demand_file)
@@ -103,13 +113,13 @@ supervisor = Supervisor(mlgraph,
                          flow_motor,
                          decision_model)
 
-
 ######################
 ### Run simulation ###
 ######################
 
 # set_all_mnms_logger_level(LOGLEVEL.WARNING)
 set_mnms_logger_level(LOGLEVEL.INFO, ["mnms.simulation"])
+
 # get_logger("mnms.graph.shortest_path").setLevel(LOGLEVEL.WARNING)
 attach_log_file(log_file)
 
