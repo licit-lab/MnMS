9f46af81783b29c73359291d11cd8482d7195239
Christophe Tettarassar <tetta.chris@gmail.com>
2025-01-07 15:55:40 +0100

    Test run_velov.py with loading stations
    Test run_velov.py with loading stations directly from lyon velov csv data

--- examples/vehicle_sharing/run_velov.py
+++ examples/vehicle_sharing/run_velov.py
@@ -4,6 +4,7 @@
 ## Casuals
 import os
 import pathlib
+import pandas as pd
 
 # MnMS
 from mnms.tools.observer import CSVUserObserver, CSVVehicleObserver
@@ -16,13 +17,15 @@ from mnms.demand import CSVDemandManager
 from mnms.time import Time, Dt
 from mnms.log import set_mnms_logger_level, LOGLEVEL, attach_log_file
 from mnms.io.graph import load_graph, load_odlayer, save_odlayer, save_transit_link_odlayer, load_transit_links
-
+from unidecode import unidecode
+from pyproj import Transformer
 
 ##################
 ### Parameters ###
 ##################
 b_from_json = False
 mlgraph_file = 'lyon_velov.json'
+lyon_velov_csv_file = 'stations-velo-v-metropole-lyon.csv'
 demand_file = pathlib.Path(__file__).parent.joinpath('fichier_demandes.csv').resolve()
 log_file = 'sim.log'
 n_nodes_per_dir = 3
@@ -43,6 +46,24 @@ affectation_factor = 10
 NX = 100
 NY = 100
 #DIST_CONNECTION = 1e2
+
+
+def gps_to_lambert93(lat, lon):
+    # Create a transformer for EPSG:4326 (WGS84) to EPSG:5698 (Lambert 93 CC46)
+    transformer = Transformer.from_crs("epsg:4326", "epsg:5698", always_xy=True)
+
+    # Transform coordinates from WGS84 to Lambert 93
+    x, y = transformer.transform(lon, lat)
+
+    return x, y
+
+
+def cleanString(string):
+
+    clean_str = unidecode(str(''.join(i for i in string if i.isalnum())))
+
+    return clean_str
+
 
 #########################
 ### Scenario creation ###
@@ -72,6 +93,25 @@ decision_model = DummyDecisionModel(mlgraph, outfile="paths.csv")
 velov_service = VehicleSharingMobilityService("velov", b_freefloating, velov_dt_matching)
 velov_service.attach_vehicle_observer(CSVVehicleObserver("velov_vehs.csv"))
 mlgraph.layers["velov_layer"].add_mobility_service(velov_service)
+
+# Velov Data
+dtype = {'idstation': str}
+df_velov = pd.read_csv(lyon_velov_csv_file, sep=';', dtype=dtype)
+df_velov = df_velov[["idstation", "nom", "nbbornettes", "lon", "lat"]]
+
+
+# Register nodes
+for index, velov_station in df_velov.iterrows():
+    node_station_name = "VLV_" + cleanString(velov_station["nom"])
+    df_velov.at[index, "nom"] = node_station_name
+
+# Add stations
+for index, velov_station in df_velov.iterrows():
+    id_station = "S" + velov_station["idstation"]
+    node_station = velov_station["nom"]
+    capacity_station = velov_station["nbbornettes"]
+
+    mlgraph.layers['velov_layer'].mobility_services['velov'].create_station(id_station, node_station, capacity=capacity_station, nb_initial_veh=5)
 
 #### Flow motor ####
 flow_motor = MFDFlowMotor(outfile="flow.csv")
